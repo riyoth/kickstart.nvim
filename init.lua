51d43ceda2b0b7212b9f536514d588d6aa9db915
Pier-Olivier Clement <poclement@riyoth.info>
2026-02-14 13:35:41 -0500

    update treesitter and ruff

--- init.lua
+++ init.lua
@@ -620,7 +620,7 @@ P.S. You can delete this when you're done too. It's your config now! :)
         local servers = {
           -- clangd = {},
           gopls = {},
-          ruff_lsp = {
+          ruff = {
             settings = {
               organizeImports = false,
             },
@@ -902,49 +902,15 @@ P.S. You can delete this when you're done too. It's your config now! :)
       end,
     },
     { -- Highlight, edit, and navigate code
-      'nvim-treesitter/nvim-treesitter',
-      build = ':TSUpdate',
-      opts = {
-        ensure_installed = {
-          'bash',
-          'c',
-          'html',
-          'lua',
-          'luadoc',
-          'markdown',
-          'vim',
-          'vimdoc',
-          'python',
-          'toml',
-          'rst',
-          'ninja',
-          'markdown',
-          'markdown_inline',
-        },
-        -- Autoinstall languages that are not installed
-        auto_install = true,
-        highlight = {
-          enable = true,
-          -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-          --  If you are experiencing weird indenting issues, add the language to
-          --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-          additional_vim_regex_highlighting = { 'ruby' },
-        },
-        indent = { enable = true, disable = { 'ruby' } },
-      },
-      config = function(_, opts)
-        -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-
-        ---@diagnostic disable-next-line: missing-fields
-        require('nvim-treesitter.configs').setup(opts)
-
-        -- There are additional nvim-treesitter modules that you can use to interact
-        -- with nvim-treesitter. You should go explore a few and see what interests you:
-        --
-        --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-        --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-        --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-      end,
+    'nvim-treesitter/nvim-treesitter',
+    config = function()
+      local filetypes = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' }
+      require('nvim-treesitter').install(filetypes)
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = filetypes,
+        callback = function() vim.treesitter.start() end,
+      })
+    end,
     },
 
     -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
